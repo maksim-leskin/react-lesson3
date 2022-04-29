70c761372b594b55aa961af99e53d5d05aeca067
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-04-29 20:56:16 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ComponentClass from "./components/ClassComponent";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <ComponentClass/>
+            </div>
+        );
+    }
+}
---
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './ClassComponent.module.css';
+
+export class ClassComponent extends React.Component{
+  render(){
+    return (
+      <div  className={style.game}>
+        <p className={style.result}>5</p>
+        <form className={style.form}>
+          <label className={style.label} htmlFor="user_number">Угадай число</label>
+          <input className={style.input} type="number" id='user_number'/>
+          <button className={style.btn}>Угадать</button>
+        </form>
+      </div>
+    )
+  }
+}
